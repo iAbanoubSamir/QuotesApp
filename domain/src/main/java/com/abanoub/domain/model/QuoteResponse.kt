486581ec18df67dafb16c0efb7395f2763cd48d0
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-01-12 02:10:12 +0200

    Create Response Model

---
+++ domain/src/main/java/com/abanoub/domain/model/QuoteResponse.kt
@@ -0,0 +1,10 @@
+package com.abanoub.domain.model
+
+data class QuoteResponse(
+    val count: Int,
+    val lastItemIndex: Int,
+    val page: Int,
+    val results: List<Result>,
+    val totalCount: Int,
+    val totalPages: Int
+)
