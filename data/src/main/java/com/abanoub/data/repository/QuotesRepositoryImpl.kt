7bcbdc6ebcede79ede425c76be2c196394a5b89a
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-01-22 21:13:25 +0200

    Implement Quotes Repository

---
+++ data/src/main/java/com/abanoub/data/repository/QuotesRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.abanoub.data.repository
+
+import com.abanoub.data.remote.QuotesApiService
+import com.abanoub.domain.model.QuoteResponse
+import com.abanoub.domain.repository.QuotesRepository
+
+class QuotesRepositoryImpl(private val apiService: QuotesApiService) : QuotesRepository {
+
+    override fun getQuotesFromAPI(): QuoteResponse = apiService.getQuotes()
+
+}
