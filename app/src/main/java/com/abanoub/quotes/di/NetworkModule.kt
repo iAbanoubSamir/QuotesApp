1139be970369743108e455fc15dd8e1153b94c26
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-01-24 00:04:56 +0200

    Implement Network Hilt Module

---
+++ app/src/main/java/com/abanoub/quotes/di/NetworkModule.kt
@@ -0,0 +1,43 @@
+package com.abanoub.quotes.di
+
+import com.abanoub.data.remote.QuotesApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.quotable.io/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuotesApiService(retrofit: Retrofit): QuotesApiService {
+        return retrofit.create(QuotesApiService::class.java)
+    }
+
+}
