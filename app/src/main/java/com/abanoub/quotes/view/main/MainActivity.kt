30917f669dcea0122b23e692714ba864e3f37f9d
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-02-08 07:18:21 +0200

    Setup UI & RecyclerView

--- app/src/main/java/com/abanoub/quotes/view/main/MainActivity.kt
+++ app/src/main/java/com/abanoub/quotes/view/main/MainActivity.kt
@@ -1,12 +1,47 @@
 package com.abanoub.quotes.view.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.abanoub.quotes.R
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.abanoub.quotes.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val quotesAdapter = QuotesAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initQuotesRecycleView()
+
+        observeForQuotesResponse()
+    }
+
+    private fun initQuotesRecycleView() {
+        binding.quotesRecyclerView.apply {
+            adapter = quotesAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun observeForQuotesResponse() {
+        viewModel.getQuotes()
+        lifecycleScope.launch {
+            viewModel.quotes.collect { response ->
+                quotesAdapter.submitList(response?.results)
+            }
+        }
+
     }
 }
